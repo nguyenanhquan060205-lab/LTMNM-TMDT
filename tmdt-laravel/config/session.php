57a51946e954ef47be33a4fe0fd096d4af17eed6
NguyenQuan <nguyenanhquan060205@gmail.com>
2026-07-28 16:49:25 +0700

    fix: force session driver to database for reliable persistence on Render

--- tmdt-laravel/config/session.php
+++ tmdt-laravel/config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('FORCE_SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
